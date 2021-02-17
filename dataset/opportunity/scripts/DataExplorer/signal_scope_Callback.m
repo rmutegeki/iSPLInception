3354d91b9f6371e1d8e648ec0d3dba22f2890f51
Ronald Mutegeki <murogive@gmail.com>
2021-02-17 16:01:26 +0900

    Updated the Opportunity dataset
    Removed the Matlab Scripts

--- dataset/opportunity/scripts/DataExplorer/signal_scope_Callback.m
+++
@@ -1,5 +0,0 @@
-function signal_scope_Callback(hObject, eventdata, guiobject)
-handles = guidata(guiobject);
-handles.sensor_scope = 0;
-guidata(guiobject, handles);
-delete(hObject); % use "delete" instead of "close" to prevent the callback for "close" being called recursively!
--- dataset/opportunity/scripts/DataExplorer/underscore_clean.m
+++
@@ -1,4 +0,0 @@
-function [ string_out ] = underscore_clean( string_in )
-string_in(strfind(string_in, '_')) = '-';
-string_out = string_in;
-end
